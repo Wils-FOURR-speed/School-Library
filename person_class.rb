6a18d764f4ca26b26005c91b662549305812705e
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-02 11:05:32 +0100

    Make changes

--- person_class.rb
+++ person_class.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(id, age, parent_permission = true, name = "Unknown")
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(id, age, parent_permission = true, name = 'Unknown')
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        of_age? || @parent_permission
-      end
-    
-      private
-    
-      def of_age?
-        @age >= 18
-      end
-    end      
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
--- teacher_class.rb
+++ teacher_class.rb
@@ -1,10 +1,12 @@
+require_relative 'person'
+
 class Teacher < Person
-    def initialize(age, parent_permission = true, name = "Unknown", specialization)
-      super(age, parent_permission, name)
-      @specialization = specialization
-    end
-  
-    def can_use_services?
-      true
-    end
+  def initialize(id, age, parent_permission = true, name = 'Unknown', specialization)
+    super(age, parent_permission, name)
+    @specialization = specialization
   end
+
+  def can_use_services?
+    true
+  end
+end
